557d5a723cda4f71022b555712791ae7705d50d5
totti-rdz <thorsten@achtsamkeitsakademie.de>
2022-11-02 16:46:02 +0100

    move StatusBar outside of NavigationContainer

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
+import SecondaryScreen from "./screens/SecondaryScreen";
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <>
       <StatusBar style="auto" />
-      <HomeScreen />
-    </NavigationContainer>
+      <NavigationContainer>
+        <HomeScreen />
+      </NavigationContainer>
+    </>
   );
 }
