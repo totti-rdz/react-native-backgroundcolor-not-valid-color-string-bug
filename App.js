7efc9134c9c42a858145c7027a48ebc4110a20d9
totti-rdz <thorsten@achtsamkeitsakademie.de>
2022-11-02 16:47:49 +0100

    setup native stack navigation

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import SecondaryScreen from "./screens/SecondaryScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <HomeScreen />
+        <Stack.Navigator>
+          <Stack.Screen name="home" component={HomeScreen} options={{ title: "Home" }} />
+          <Stack.Screen name="secondary" component={SecondaryScreen} options={{ title: "Secondary" }} />
+        </Stack.Navigator>
       </NavigationContainer>
     </>
   );
